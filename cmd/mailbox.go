533ae95bada627ef57137dcaac901762bd9fc248
Viktor Chukhantsev <email@vch.su>
2023-02-16 02:46:49 +0700

    Add create mailbox command.

--- cmd/mailbox.go
+++ cmd/mailbox.go
@@ -8,6 +8,7 @@ import (
 
 func init() {
 	mailboxCmd.AddCommand(mailboxLsCmd)
+	mailboxCmd.AddCommand(mailboxCreateCmd)
 	rootCmd.AddCommand(mailboxCmd)
 }
 
@@ -27,5 +28,15 @@ var mailboxLsCmd = &cobra.Command{
 	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		app.ListMailboxes(args[0])
+	},
+}
+
+var mailboxCreateCmd = &cobra.Command{
+	Use:   "create",
+	Short: "create mailboxes",
+	Long:  "Create mailboxes on server",
+	Args:  cobra.MinimumNArgs(2),
+	Run: func(cmd *cobra.Command, args []string) {
+		app.CreateMailbox(args[0], args[1:]...)
 	},
 }
